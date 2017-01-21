a23d23a4b3c0cc02839e480eaffbbd822cee40ad
Rice Shelley <rootieDev@gmail.com>
2017-01-21 17:34:59 -0600

    were going to win

---
+++ problems/Input.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package problems;
+
+/**
+ *
+ * @author rootie
+ */
+public class Input {
+    // change this to match the path on ur computer parker
+    // this way we only have to change this varible everytime 
+    public static String PATH = "/home/rootie/softwareDev/codeQuest/CodeQuest/src/ExampleInputs/";
+}
